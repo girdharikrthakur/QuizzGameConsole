eb00f3d8d26bdd6073cff1c1d0b47652ed472eb9
Evil Hound <girdharikumar101@gmail.com>
2024-11-30 12:47:07 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        QuestionService questionService=new QuestionService();
+
+        questionService.displayQuestions();
+        questionService.printScore();
+    }
+}
---
+++ src/QuestionService.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class QuestionService {
+
+Question[] questions =new Question[5];
+String input[] = new String[5];
+
+public QuestionService()
+{
+questions[0] =new Question(1,"what", "Java","Cpp","Python","C","1");
+questions[1] =new Question(2,"what", "Java","Cpp","Python","C","1");
+questions[2] =new Question(3,"what", "Java","Cpp","Python","C","1");
+questions[3] =new Question(4,"what", "Java","Cpp","Python","C","1");
+questions[4] =new Question(5,"what", "Java","Cpp","Python","C","1");
+
+}
+
+public void displayQuestions()
+{
+    int i=0;
+for(Question q:questions){
+    System.out.println("Question no. : "+q.getId());
+    System.out.println(q.getQuestion());
+    System.out.println("Option 1: "+ q.getOpt1());
+    System.out.println("Option 2: "+ q.getOpt2());
+    System.out.println("Option 3: "+ q.getOpt3());
+    System.out.println("Option 4: "+ q.getOpt4());
+    Scanner scn =new Scanner(System.in);
+    input[i]=scn.nextLine();
+    i++;
+
+}
+for (String s : input) {
+ 
+    System.out.println("Your Selected Option for Question No: "+getId()+" is "+s);
+  
+}
+
+}
+
+public void printScore(){
+    int score =0;
+    for(int i=0;i<questions.length;i++){
+        Question ques =questions[i];
+        String answer=ques.getAnswers();
+        String userAnswer=input[i];
+        if (answer.equals(userAnswer)) {
+            score++;
+        }
+       
+
+    }
+    System.out.println("Your Score is : "+score);
+}
+
+}
